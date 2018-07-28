d410205849b850cd2404329127f456ed025b8c27
Nico <nicoargiz@icloud.com>
2018-07-28 03:39:58 -0300

    Se achico la navbar. Links desde 'que hacemos'. Tamaño y responsive imagen minita diseño. Acomodo formulario de contacto y se configura PHP, ya funciona mandando mail

--- proceso.php
+++ proceso.php
@@ -1,20 +1,18 @@
-
-<?php 
-if(isset($_POST['submit'])){
-    $to = "nicoargiz@icloud.com"; // this is your Email address
-    $from = $_POST['email']; // this is the sender's Email address
-    $first_name = $_POST['nombre'];
-    $email = $_POST['email'];
-    $subject = "Form submission";
-    // $subject2 = "Copy of your form submission";
-    $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['message'];
-    // $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
-
-    $headers = "From:" . $from;
-    // $headers2 = "From:" . $to;
-    mail($to,$subject,$message,$headers);
-    // mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
-    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
-    // You can also use header('Location: thank_you.php'); to redirect to another page.
-    }
+<?php
+$to = "nicoargiz@hotmail.com"; // This is your Email address
+$from = $_POST['email']; // This is the sender's Email address
+$first_name = $_POST['nombre']; // This is the sender's Name
+$company = $_POST['empresa'];
+$phone = $_POST['telefono'];
+$comments = $_POST['mensaje'];
+$subject = "Email de KCM form";
+$comments = "Nombre: $first_name \n
+            Empresa: $company \n
+            Telefono: $phone \n
+            Mensaje: $comments ";
+$headers = "From:" . $from;
+$email = mail($to,$subject,$comments,$headers);
+if(!$email){
+  echo "Hubo un error al procesar tu email, detallle del error". $email; 
+}
 ?>
